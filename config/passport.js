62930d124ea20b1f7e27ace8a8cbc02970e4bb6d
Rafael Menon <rafaelmenon@localhost.localdomain>
2020-06-13 11:50:44 -0300

    crud.

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const { authSecret } = require("../.env");
+const { authSecret } = require("../.keySecret");
 const passport = require("passport");
 const passportJwt = require("passport-jwt");
 const { Strategy, ExtractJwt } = passportJwt;
