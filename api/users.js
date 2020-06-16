455560a11dd98afab582198b50b138fe315f8429
Rafael Menon <rafael13rodrigo@gmail.com>
2020-06-16 12:39:27 -0300

    settings

--- api/users.js
+++ api/users.js
@@ -2,11 +2,11 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 
 module.exports = (app) => {
-  const { existsOrError, notExistsOrError, equalOrError } = app.api.validation;
+  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation;
 
   const encryptPassword = (password) => {
     const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(passport, salt);
+    return bcrypt.hashSync(password, salt);
   };
 
   const save = async (req, res) => {
@@ -15,11 +15,13 @@ module.exports = (app) => {
     if (req.params.id) user.id = req.params.id;
 
     try {
-      existsOrError(user.name, "Necessário informar o nome");
-      existsOrError(user.email, "Necessário informa o email");
+      existsOrError(user.name, "Nome não informado");
+      existsOrError(user.email, "E-mail não informado");
+      existsOrError(user.phone, "Telefone não informado");
+      existsOrError(user.city, "Cidade não informada");
       existsOrError(user.password, "Senha não informada");
       existsOrError(user.confirmPassword, "Confirmação de senha inválida");
-      equalOrError(user.password, user.confirmPassword, "Senhas não conferem");
+      equalsOrError(user.password, user.confirmPassword, "Senhas não conferem");
 
       const userFromDB = await app
         .db("users")
@@ -41,7 +43,8 @@ module.exports = (app) => {
         .db("users")
         .update(user)
         .where({ id: user.id })
-        .then((_) => res.status(204).send("Usuário atualizado com sucesso!"));
+        .then((_) => res.status(204).send("Usuário atualizado com sucesso"))
+        .catch(res.status(500).send(err));
     } else {
       app
         .db("users")
@@ -51,12 +54,23 @@ module.exports = (app) => {
     }
   };
 
-  const get = (req, res) => {
-    app
-      .db("users")
-      .select("id", "name", "email", "avatar", "city", "phone")
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).send(err));
+  const get = async (req, res) => {
+    const user = { ...req.params };
+
+    if (user.id) {
+      app
+        .db("users")
+        .select("id", "name", "email", "phone", "city")
+        .where({ id: req.params.id })
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).send(err));
+    } else {
+      app
+        .db("users")
+        .select("id", "name", "email", "phone", "city")
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).send(err));
+    }
   };
 
   const remove = async (req, res) => {
--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,11 @@ module.exports = (app) => {
   app.post("/login", app.api.auth.login);
   app.post("/validateToken", app.api.auth.validateToken);
 
+  app.route("/users").post(app.api.users.save).get(app.api.users.get);
+
   app
-    .route("/users")
-    .post(app.api.users.save)
+    .route("/user/:id")
     .put(app.api.users.save)
-    .delete(app.api.users.remove)
-    .get(app.api.users.get);
+    .get(app.api.users.get)
+    .delete(app.api.users.remove);
 };
